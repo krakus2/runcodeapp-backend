23a3f8eef1ab24feb4b6def2d13efb17c21b776b
Filip Krakowiak <filip.krakowiak@ynd.co>
2020-09-28 23:40:46 +0200

    Moved SQL db configuration to env variables

--- startup/sqldb.js
+++ startup/sqldb.js
@@ -1,19 +1,19 @@
-const mysql = require('mysql');
-const config = require('config');
+const mysql = require('mysql')
+const config = require('config')
 
 const db = mysql.createConnection({
-   host: 'db4free.net',
-   user: 'runcodeapp',
-   password: config.get('sqlPassword'),
-   database: 'runcodeapp'
-});
+  host: config.get('sqlHost') || 'db4free.net',
+  user: config.get('sqlUser') || 'runcodeapp',
+  password: config.get('sqlPassword'),
+  database: config.get('sqlDatabase') || 'runcodeapp',
+})
 
-db.connect(function(err) {
-   if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-   }
-   console.log('connected as id ' + db.threadId);
-});
+db.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack)
+    return
+  }
+  console.log('connected as id ' + db.threadId)
+})
 
-module.exports = db;
+module.exports = db
