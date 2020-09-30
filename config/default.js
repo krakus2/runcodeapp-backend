05ec1fc49dd1b270529ed5781821021e5a1fa36c
Filip Krakowiak <filip.krakowiak@ynd.co>
2020-09-30 16:35:47 +0200

    Get rid of unused routing

--- config/default.js
+++ config/default.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 
 module.exports = {
-  mongoUrl: process.env.MONGOURL_PROD,
   sqlHost: process.env.SQL_HOST,
   sqlUser: process.env.SQL_USER,
   sqlDatabase: process.env.SQL_DATABASE,
--- index.js
+++ index.js
@@ -11,16 +11,6 @@ require('express-async-errors')
 require('./startup/logging')()
 require('./startup/routes')(app)
 
-if (process.env.NODE_ENV === 'production') {
-  //set static folder
-  const buildPath = path.join(__dirname, '../', 'frontend', 'build')
-  app.use(express.static(buildPath))
-  //for any route that isn't hit with routes/api use this route
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(buildPath, 'index.html'))
-  })
-}
-
 const port = process.env.PORT || 5000
 
 const server = app.listen(port, () => {
